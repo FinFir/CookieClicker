fec32e790ca02a2c71d789287c1eba53bc8bedb4
FinFir <150492849+FinFir@users.noreply.github.com>
2024-02-16 17:47:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let cookies = 0;
+let clickValue = 1;
+let upgradeCost = 10;
+let autoClickerCost = 50;
+let autoClickerInterval;
 
+function clickCookie() {
+  cookies += clickValue;
+  updateCookiesDisplay();
+}
+
+function buyUpgrade() {
+  if (cookies >= upgradeCost) {
+    cookies -= upgradeCost;
+    clickValue *= 2;
+    upgradeCost *= 2;
+    updateCookiesDisplay();
+  }
+}
+
+function buyAutoClick() {
+  if (cookies >= autoClickerCost) {
+    cookies -= autoClickerCost;
+    startAutoClicker();
+    autoClickerCost *= 2;
+    updateCookiesDisplay();
+  }
+}
+
+function startAutoClicker() {
+  autoClickerInterval = setInterval(function() {
+    clickCookie();
+  }, 1000);
+}
+
+function updateCookiesDisplay() {
+  document.getElementById('cookies').innerText = `${cookies} cookies`;
+}
