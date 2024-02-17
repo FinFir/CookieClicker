2c3057b2940785b6c715d9f7ae0c56b7aab9787d
FinFir <150492849+FinFir@users.noreply.github.com>
2024-02-16 18:06:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let cookies = 0;
 let clickValue = 1;
 let upgradeCost = 10;
 let autoClickerCost = 50;
-let autoClickerInterval;
+let grandmaCost = 30;
+let grandmaCount = 0;
 
 function clickCookie() {
-  cookies += clickValue;
+  cookies += clickValue + grandmaCount; // Add cookies from grandma
   updateCookiesDisplay();
+  addChocolateChip();
 }
 
 function buyUpgrade() {
@@ -31,10 +33,34 @@ function buyAutoClick() {
   }
 }
 
+function buyGrandma() {
+  if (cookies >= grandmaCost) {
+    cookies -= grandmaCost;
+    grandmaCount++;
+    grandmaCost = Math.round(grandmaCost * 1.5); // 50% more expensive
+    updateCookiesDisplay();
+  } else {
+    alert(`Not Enough Cookies. You need ${grandmaCost - cookies} more to get a Grandma!`);
+  }
+}
+
 function startAutoClicker() {
   autoClickerInterval = setInterval(function () {
     clickCookie();
   }, 1000);
+}
+
+function addChocolateChip() {
+  const chocolateChip = document.createElement('div');
+  chocolateChip.className = 'chocolate-chip';
+  const cookie = document.getElementById('cookie');
+  const chipPosition = {
+    top: Math.random() * 200,
+    left: Math.random() * 200,
+  };
+  chocolateChip.style.top = `${chipPosition.top}px`;
+  chocolateChip.style.left = `${chipPosition.left}px`;
+  cookie.appendChild(chocolateChip);
 }
 
 function updateCookiesDisplay() {
