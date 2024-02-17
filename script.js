4cbbd238154392c09833e411db42ca010beec3f4
FinFir <150492849+FinFir@users.noreply.github.com>
2024-02-16 20:54:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let cookies = 0;
 let clickValue = 1;
-let upgradeCost = 10;
 let autoClickerCost = 50;
 let grandmaCost = 30;
 let grandmaCount = 0;
@@ -9,20 +8,37 @@ let cookieElement = document.getElementById('cookie');
 cookieElement.addEventListener('click', clickCookie);
 
 function clickCookie() {
+  playClickSound();
   cookies += clickValue + grandmaCount; // Add cookies from grandma
   updateCookiesDisplay();
-  addChocolateChip();
+  addFallingCookie();
 }
 
-function buyUpgrade() {
-  if (cookies >= upgradeCost) {
-    cookies -= upgradeCost;
-    clickValue *= 2;
-    upgradeCost = Math.round(upgradeCost * 1.5); // 50% more expensive
-    updateCookiesDisplay();
-  } else {
-    alert(`Not Enough Cookies. You need ${upgradeCost - cookies} more!`);
-  }
+function playClickSound() {
+  // Add code to play a click sound (e.g., using an <audio> element or a library)
+  // Example: const clickSound = new Audio('path/to/click-sound.mp3');
+  // clickSound.play();
+}
+
+function addFallingCookie() {
+  const fallingCookie = document.createElement('div');
+  fallingCookie.className = 'falling-cookie';
+  document.body.appendChild(fallingCookie);
+
+  const cookieEmoji = document.createElement('span');
+  cookieEmoji.innerHTML = '🍪';
+  fallingCookie.appendChild(cookieEmoji);
+
+  let position = 0;
+  const fallInterval = setInterval(() => {
+    position += 5;
+    fallingCookie.style.bottom = `${position}px`;
+
+    if (position >= window.innerHeight) {
+      clearInterval(fallInterval);
+      document.body.removeChild(fallingCookie);
+    }
+  }, 20);
 }
 
 function buyAutoClick() {
@@ -52,18 +68,6 @@ function startAutoClicker() {
   autoClickerInterval = setInterval(function () {
     clickCookie();
   }, 1000);
-}
-
-function addChocolateChip() {
-  const chocolateChip = document.createElement('div');
-  chocolateChip.className = 'chocolate-chip';
-  const chipPosition = {
-    top: Math.random() * 200,
-    left: Math.random() * 200,
-  };
-  chocolateChip.style.top = `${chipPosition.top}px`;
-  chocolateChip.style.left = `${chipPosition.left}px`;
-  cookieElement.appendChild(chocolateChip);
 }
 
 function addGrandma() {
